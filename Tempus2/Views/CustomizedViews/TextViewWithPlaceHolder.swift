5c6ad9811bd9f747425d81f41ac573856b8fdc15
Chunngai <mikumikku3939@icloud.com>
2022-04-29 18:22:24 +0800

    add clear button

--- Tempus2/Views/CustomizedViews/TextViewWithPlaceHolder.swift
+++ Tempus2/Views/CustomizedViews/TextViewWithPlaceHolder.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class TextViewWithPlaceHolder: UITextView {
     
+    var clearButton: UIButton = {
+        let clearButton = UIButton(type: .custom)
+        clearButton.setImage(UIImage(imageLiteralResourceName: "clear"), for: .normal)
+        clearButton.setImage(UIImage(imageLiteralResourceName: "clear_pressed"), for: .highlighted)
+        return clearButton
+    }()
+    
     var placeHolder: String?
     var isShowingPlaceHolder: Bool {
         guard let placeHolder = placeHolder else {
@@ -35,6 +42,8 @@ class TextViewWithPlaceHolder: UITextView {
                 text = placeHolder
                 textColor = Theme.placeHolderColor
                 selectBeginning()
+                
+                clearButton.isHidden = true
             } else {
                 text = isShowingPlaceHolder
                     // textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool
@@ -43,6 +52,8 @@ class TextViewWithPlaceHolder: UITextView {
                     ? ""
                     : newValue
                 textColor = Theme.textColor
+                
+                clearButton.isHidden = false
             }
         }
     }
@@ -51,10 +62,32 @@ class TextViewWithPlaceHolder: UITextView {
     
     override init(frame: CGRect, textContainer: NSTextContainer?) {
         super.init(frame: frame, textContainer: textContainer)
+        
+        clearButton.addTarget(self, action: #selector(clearButtonTapped), for: .touchUpInside)
+        self.addSubview(clearButton)
+        
+        updateViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        clearButton.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().inset(self.frame.width - TextViewWithPlaceHolder.clearButtonDiameter * 2)
+            make.centerY.equalToSuperview()
+            make.width.height.equalTo(TextViewWithPlaceHolder.clearButtonDiameter)
+        }
+    }
+    
+    func updateViews() {
+        textContainerInset = UIEdgeInsets(
+            top: textContainerInset.top,
+            left: textContainerInset.left,
+            bottom: textContainerInset.bottom,
+            right: TextViewWithPlaceHolder.clearButtonDiameter * 2
+        )
     }
 }
 
@@ -67,4 +100,19 @@ extension TextViewWithPlaceHolder {
         }
         return bool
     }
+}
+
+extension TextViewWithPlaceHolder {
+    
+    // MARK: - Actions
+    
+    @objc func clearButtonTapped() {
+        content = ""
+    }
+}
+
+extension TextViewWithPlaceHolder {
+    
+    static let clearButtonDiameter: CGFloat = 15
+    
 }
